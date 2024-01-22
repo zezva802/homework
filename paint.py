aaee9989031909bcf5c6542cedfcb82084a96089
zezva <kobaidze.zezva@kiu.edu.ge>
2024-01-22 11:58:17 +0400

    paintgame

--- paint.py
+++ paint.py
@@ -1,1 +1,17 @@
-print("12345")
+import pygame
+
+pygame.init()
+
+width = 500
+height = 600
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Paint")
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
